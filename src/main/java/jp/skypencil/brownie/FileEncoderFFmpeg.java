c967569337445944c74b04a5ef118f3ae429c847
Kengo TODA <skypencil@gmail.com>
2015-11-06 11:46:21 +0800

    improve documentation in FileEncoder.java

---
+++ src/main/java/jp/skypencil/brownie/FileEncoderFFmpeg.java
@@ -0,0 +1,71 @@
+package jp.skypencil.brownie;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+
+import javax.annotation.Resource;
+
+import jp.skypencil.brownie.fs.DistributedFileSystem;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class FileEncoderFFmpeg implements FileEncoder {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Resource
+    private Vertx vertx;
+
+    @Resource
+    private DistributedFileSystem fileSystem;
+
+    @Override
+    public void convert(File targetFile, String resolution, Handler<AsyncResult<File>> handler) {
+        final int processors = Runtime.getRuntime().availableProcessors();
+        vertx.executeBlocking(
+                convert(targetFile, resolution, processors),
+                true,
+                handler);
+    }
+
+    private Handler<Future<File>> convert(File targetFile,
+            String resolution, final int processors) {
+        return future -> {
+            logger.info("Converting {} to {}", targetFile, resolution);
+            String resultFileName = targetFile.getAbsolutePath() + "-" + resolution + ".webm";
+            ProcessBuilder builder = new ProcessBuilder().command("ffmpeg",
+                    "-i", targetFile.getAbsolutePath(),
+                    "-s", resolution,
+                    // Vorbis encoder only supports 2 channels. http://stackoverflow.com/a/19005961
+                    "-ac", "2",
+                    // 'vorbis' is now experimental, we need following parameter to enable it.
+                    "-strict", "-2",
+                    "-n",
+                    "-threads", Integer.toString(processors * 3 / 2),
+                    resultFileName);
+            builder.redirectError();
+            builder.redirectOutput();
+            try {
+                Process process = builder.start();
+                int statusCode;
+                if ((statusCode = process.waitFor()) != 0) {
+                    throw new IllegalStateException("FFmpeg failed with illegal status code: " + statusCode);
+                }
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            } catch (InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
+            future.complete(new File(resultFileName));
+        };
+    }
+}
