e3cc73e2e30173c8f17ee51cf148d22d28943e31
Kengo TODA <skypencil@gmail.com>
2015-12-05 08:54:30 +0800

    create DnsClient to autowire

--- src/main/java/jp/skypencil/brownie/Application.java
+++ src/main/java/jp/skypencil/brownie/Application.java
@@ -3,6 +3,7 @@ package jp.skypencil.brownie;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
+import io.vertx.core.dns.DnsClient;
 import io.vertx.core.spi.cluster.ClusterManager;
 import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager;
 
@@ -39,6 +40,12 @@ public class Application {
 
     @Value("${BROWNIE_MOUNTED_DIR:/mnt/brownie}")
     private String mountedDirectory;
+
+    @Value("${BROWNIE_DNS_HOST:'localhost'}")
+    private String dnsHost;
+
+    @Value("${BROWNIE_DNS_PORT:53}")
+    private int dnsPort;
 
     private Future<Vertx> vertxFuture;
 
@@ -109,6 +116,11 @@ public class Application {
         vertx = vertxFuture.result();
         vertx.eventBus().registerDefaultCodec(Task.class, new TaskCodec());
         return vertx;
+    }
+
+    @Bean
+    public DnsClient dnsClient() throws InterruptedException {
+        return vertx().createDnsClient(dnsPort, dnsHost);
     }
 
     @Bean
