66cc485f432530372f3471794c44298b59e8d47b
Kengo TODA <skypencil@gmail.com>
2016-07-27 06:52:15 +0800

    fix test cases

--- src/test/java/jp/skypencil/brownie/registry/FileMetadataRegistryOnPostgresTest.java
+++ src/test/java/jp/skypencil/brownie/registry/FileMetadataRegistryOnPostgresTest.java
@@ -2,12 +2,22 @@ package jp.skypencil.brownie.registry;
 
 import org.junit.runner.RunWith;
 
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
 
 @RunWith(VertxUnitRunner.class)
 public class FileMetadataRegistryOnPostgresTest extends FileMetadataRegistryTest {
     @Override
     FileMetadataRegistry createRegistry() {
-        return new FileMetadataRegistryOnPostgres(System.getProperty("db.host", "localhost"), vertx);
+        return new FileMetadataRegistryOnPostgres(vertx, postgresConfig());
+    }
+
+    private JsonObject postgresConfig() {
+        return new JsonObject()
+                .put("host", System.getProperty("db.host", "localhost"))
+                .put("port", System.getProperty("db.port", "5432"))
+                .put("username", "brownie")
+                .put("password", "brownie")
+                .put("database", "brownie");
     }
 }
