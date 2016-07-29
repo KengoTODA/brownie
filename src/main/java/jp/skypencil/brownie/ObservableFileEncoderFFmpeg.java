ffe618bb789fc2f98cdc923daa149b2ecd89da2e
Kengo TODA <skypencil@gmail.com>
2016-07-29 21:51:42 +0800

    create ObservableFileEncoder

---
+++ src/main/java/jp/skypencil/brownie/ObservableFileEncoderFFmpeg.java
@@ -0,0 +1,73 @@
+package jp.skypencil.brownie;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Component;
+
+import io.vertx.core.Handler;
+import io.vertx.rxjava.core.Future;
+import io.vertx.rxjava.core.Vertx;
+import jp.skypencil.brownie.fs.ObservableSharedFileSystem;
+import lombok.extern.slf4j.Slf4j;
+import rx.Observable;
+
+/**
+ * An {@link ObservableFileEncoder} implementation which depends on FFmpeg.
+ * It needs {@code ffmpeg} executable in the {@code PATH}.
+ */
+@Component
+@Slf4j
+@ParametersAreNonnullByDefault
+public class ObservableFileEncoderFFmpeg implements ObservableFileEncoder {
+    @Resource
+    private Vertx vertx;
+
+    @Resource
+    private ObservableSharedFileSystem fileSystem;
+
+    @Override
+    public Observable<File> convert(File targetFile, String resolution) {
+        Objects.requireNonNull(targetFile);
+        Objects.requireNonNull(resolution);
+        final int processors = Runtime.getRuntime().availableProcessors();
+        return vertx.executeBlockingObservable(
+                convert(targetFile, resolution, processors));
+    }
+
+    private Handler<Future<File>> convert(File targetFile,
+            String resolution, final int processors) {
+        return future -> {
+            log.info("Converting {} to {}", targetFile, resolution);
+            String resultFileName = targetFile.getAbsolutePath() + "-" + resolution + ".webm";
+            ProcessBuilder builder = new ProcessBuilder().command("ffmpeg",
+                    "-i", targetFile.getAbsolutePath(),
+                    "-s", resolution,
+                    // Vorbis encoder only supports 2 channels. http://stackoverflow.com/a/19005961
+                    "-ac", "2",
+                    // 'vorbis' is now experimental, we need following parameter to enable it.
+                    "-strict", "-2",
+                    "-n",
+                    "-threads", Integer.toString(processors * 3 / 2),
+                    resultFileName);
+            builder.redirectError();
+            builder.redirectOutput();
+            try {
+                Process process = builder.start();
+                int statusCode;
+                if ((statusCode = process.waitFor()) != 0) {
+                    future.fail("FFmpeg failed with illegal status code: " + statusCode);
+                    return;
+                }
+            } catch (IOException | InterruptedException e) {
+                future.fail(e);
+                return;
+            }
+            future.complete(new File(resultFileName));
+        };
+    }
+}
